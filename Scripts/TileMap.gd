ee20c0530130832f8d87b5f7addfdd69d42e46fd
Jasper Grant <jasperbengrant@gmail.com>
2024-01-27 18:58:54 -0400

    Added dynamic camera

---
+++ Scripts/TileMap.gd
@@ -0,0 +1,15 @@
+extends TileMap
+
+@onready var multi_cam = %MultiCam
+@onready var player_1 = $"../Player1"
+@onready var player_2 = $"../Player2"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	multi_cam.add_target(player_1)
+	multi_cam.add_target(player_2)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
