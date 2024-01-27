609e236200ab1a4aa21752e90bdb3500b82d9de5
Eaocs <Owen.Stuttard@dal.ca>
2024-01-27 17:20:22 -0400

    edited button to be cleaner in the way it loads

--- scenes/play_button.gd
+++ scenes/play_button.gd
@@ -1,11 +1,11 @@
 extends Button
 
-const NEXT_SCENE_PATH: String = "scenes/arena_1.tscn"
+const NEXT_SCENE_PATH: String = "res://scenes/arena_1.tscn"
 
 func _on_pressed():
 	print(get_tree_string())
-	ResourceLoader.load_threaded_request(NEXT_SCENE_PATH)
-	var next_scene = ResourceLoader.load_threaded_get(NEXT_SCENE_PATH)
-	var next = next_scene.instantiate()
-	get_node("/root").add_child(next)
+	#ResourceLoader.load_threaded_request(NEXT_SCENE_PATH)
+	#var next_scene = ResourceLoader.load_threaded_get(NEXT_SCENE_PATH)
+	var next_scene = preload(NEXT_SCENE_PATH).instantiate()
+	get_node("/root").add_child(next_scene)
 	get_parent().queue_free()
