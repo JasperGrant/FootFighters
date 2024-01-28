fa863a3cceb2a8ab22ae86556db8f07b2cec1cba
AndrewDoucet <92753789+AndrewDoucet@users.noreply.github.com>
2024-01-28 13:14:05 -0400

    Made Death Screen Work

--- Scripts/laugh.cs
+++ Scripts/laugh.cs
@@ -8,24 +8,33 @@ public partial class laugh : Node
 
 	private TextureRect _blue;
 	private TextureRect _red;
+	private TextureRect _redFoot;
+	private TextureRect _blueFoot;
+
 
 	public void gameOver(bool blueLost)
 	{
 		if(blueLost)
 		{
 			_blue.Visible = false;
+			_blueFoot.Visible = false;
 			_red.Visible = true;
+			_redFoot.Visible = true;
 		}
 		else
 		{
 			_blue.Visible = true;
+			_blueFoot.Visible = true;
 			_red.Visible = false;
+			_redFoot.Visible = false;
 		}
 	}
 	public override void _Ready()
 	{
 		_blue = GetNode<TextureRect>("Blue");
 		_red = GetNode<TextureRect>("Red");
+		_redFoot = GetNode<TextureRect>("RedToenails");
+		_redFoot = GetNode<TextureRect>("RedToenails");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
