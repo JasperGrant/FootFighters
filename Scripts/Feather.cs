c295f6039f2c9d5dfc668a3eba7e5a01940cb433
Jasper Grant <jasperbengrant@gmail.com>
2024-01-28 00:17:28 -0400

    Added Particles

--- Scripts/Feather.cs
+++ Scripts/Feather.cs
@@ -3,6 +3,8 @@ using Godot;
 
 public partial class Feather : RigidBody2D
 {
+	
+	private PackedScene ParticleScene = GD.Load<PackedScene>("res://scenes/particles.tscn");
 
 	public int X = 5;
 	public Godot.Vector2 velocity = new(0,0);
@@ -31,8 +33,15 @@ public partial class Feather : RigidBody2D
 		if(node.GetType().ToString() == "PlayerCharacter")
 		{
 			node.GetNode<PlayerCharacter>(node.GetPath()).decrement_health(1);
+			
 		}
-		QueueFree();		
+		else{
+			var _Particle = ParticleScene.Instantiate() as GpuParticles2D;
+		_Particle.GlobalPosition = this.Position;
+		_Particle.RotationDegrees = 0;
+		GetParent().AddChild(_Particle);
+		}
+		QueueFree();
 	}
 
 	public void setVelo(float X, float Y)
---
+++ Scripts/particles.gd
@@ -0,0 +1,11 @@
+extends GPUParticles2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Hello");
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print("Hello");
